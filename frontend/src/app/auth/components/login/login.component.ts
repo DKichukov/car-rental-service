c9c7911c5dec3247930e995f2a2cc4573ad15313
DKichukov <dzhamal.kichukov@gmail.com>
2024-09-02 09:45:59 +0300

    connect login api with login form

--- frontend/src/app/auth/components/login/login.component.ts
+++ frontend/src/app/auth/components/login/login.component.ts
@@ -1,29 +1,28 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { AuthService } from "../../services/auth/auth.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent {
-
   isSpinning: boolean = false;
   loginFrom!: FormGroup;
-  
-  constructor(private fb: FormBuilder) {
-    
-  }
-  
+
+  constructor(private fb: FormBuilder, private authService: AuthService) {}
+
   ngOnInit(): void {
-   this.loginFrom = this.fb.group({
-    email: ['', [Validators.email, Validators.required]],
-    password: ['', [Validators.required]]
-   });
+    this.loginFrom = this.fb.group({
+      email: ["", [Validators.email, Validators.required]],
+      password: ["", [Validators.required]],
+    });
   }
-  login(){
-  console.log(this.loginFrom.value); 
-  
+  login() {
+    console.log(this.loginFrom.value);
+    this.authService.login(this.loginFrom.value).subscribe((data) => {
+      console.log(data);
+    });
   }
-  
 }
